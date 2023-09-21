d9c6dd27f0627ef58cd3bb4006477b697dceccdd
Samuel Muñoz Ortega <samuel3801@gmail.com>
2023-09-21 15:13:42 +0200

    new models for promotionType and Webshop, to use proper n-1 relationships

--- couponstats/models.py
+++ couponstats/models.py
@@ -14,13 +14,30 @@ class Coupon(models.Model):
     description = models.TextField()
     first_seen = models.DateField()
     last_seen = models.DateField()
-    promotion_type = models.CharField(
-        max_length=20,
+    promotion_type = models.ForeignKey(
+        'PromotionType',
+        on_delete=models.SET_NULL,
         null=True
     )
     title = models.CharField(max_length=255)
     value = models.PositiveIntegerField(null=True)
-    webshop_id = models.CharField(max_length=255)
+    webshop_id = models.ForeignKey(
+        'Webshop',
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
     def __str__(self):
         return f'{self.title} ({self.coupon_id})'
+
+class PromotionType(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+class Webshop(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
