c89279dd4f473725f28a92dad7c0d5ef28f8a941
Samuel Muñoz Ortega <samuel3801@gmail.com>
2023-09-22 00:33:40 +0200

    simpler urls, keep admin

--- couponstats/urls.py
+++ couponstats/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('count/', views.count, name='count'),
+    path('stats/count/', views.count_coupons, name='count_coupons'),
     path('stats/<str:promotion_type>', views.stats, name='stats'),
     path('stats/<str:promotion_type>/retailers', views.stats_by_retailer, name='stats_by_retailer'),
+    path('', views.all_stats, name='all_stats'),
 ]
 
