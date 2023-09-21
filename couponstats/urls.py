c388d85b2ed597a53ccc66143239c6f484b5b6ed
Samuel Muñoz Ortega <samuel3801@gmail.com>
2023-09-21 16:55:27 +0200

    urls update

--- couponstats/urls.py
+++ couponstats/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('count/', views.count, name='count'),
+    path('stats/<str:promotion_type>', views.stats, name='stats'),
+    path('stats/<str:promotion_type>/retailers', views.stats_by_retailer, name='stats_by_retailer'),
 ]
+
